68e2c82fadeed313aaa53219c2dfb74974812f96
BitaminW <114838001+BitaminW@users.noreply.github.com>
2022-12-22 20:08:47 +0900

    Update main.h

--- main.h
+++ main.h
@@ -1,1 +1,67 @@
-//
+#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stdlib.h>
+
+
+
+
+#define ull unsigned long long
+
+
+
+
+
+class BigInteger{
+private:
+	std::string m_value;
+
+public:
+	// Constructor :  
+	BigInteger();
+	BigInteger(ull n = 0);
+	BigInteger(std::string &);
+	BigInteger(const char *);
+	BigInteger(BigInteger &);
+
+
+	~BigInteger();
+
+	// Helper Functions..
+	friend void divide_by_2(BigInteger& a);
+	friend void Null(const BigInteger&);
+	friend int	Length(const BigInteger&);
+	int operator[](const int)const;
+	
+	
+
+	//	  ..Operator Overloading..  	//
+	
+
+	//	Direct assignment
+	BigInteger& operator=(const BigInteger&);
+
+
+	//	Incrementation
+	BigInteger& operator++();
+	BigInteger& operator++(int temp);
+	BigInteger& operator--();
+	BigInteger& operator--(int temp);
+
+
+	//	(Addition , Subtraction)
+	friend BigInteger& operator+=(BigInteger&, const BigInteger&);
+	friend BigInteger& operator-=(BigInteger&, const BigInteger&);
+	friend BigInteger operator+(const BigInteger&, const BigInteger&);
+	friend BigInteger operator-(const BigInteger&, const BigInteger&);
+
+
+	// Comparison operators
+
+
+
+public:
+
+
+};
